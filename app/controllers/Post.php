8ea7b526418b6bb4b65d406247bc640145c966a7
Skayo <jonas@skayo.dev>
2021-03-27 15:18:11 +0100

    Redirect directly when user is human

--- app/controllers/Post.php
+++ app/controllers/Post.php
@@ -18,6 +18,11 @@ class Post {
 		$mediaUrl = "https://www.instagram.com/p/$shortCode/";
 
 		if (!$media) {
+			if (self::isHuman($cache)) {
+				Flight::redirect($mediaUrl, 301);
+				return;
+			}
+
 			$instagram = Instagram::withCredentials(new Client(), $_ENV['INSTAGRAM_USERNAME'], $_ENV['INSTAGRAM_PASSWORD'], $cache);
 			$instagram->login();
 
@@ -38,5 +43,21 @@ class Post {
 		Flight::view()->set('description', "{$media['likesCount']} Likes, {$media['commentsCount']} Comments - {$media['owner']['fullName']} (@{$media['owner']['username']}) on Instagram: “{$media['caption']}”");
 
 		Flight::render('post');
+	}
+
+	private static function isHuman($cache) {
+		$userAgent = $_SERVER['HTTP_USER_AGENT'];
+		$cacheKey = md5($userAgent);
+
+		$isHuman = $cache->get($cacheKey);
+		if ($isHuman !== null) return $isHuman;
+
+		$browsers = require __DIR__ . '/../helpers/browsers.php';
+		$browserInfo = get_browser($userAgent, true);
+
+		$isHuman = in_array($browserInfo['browser'], $browsers);
+
+		$cache->set($cacheKey, $isHuman, 315360000); // expire in 10 years
+		return $isHuman;
 	}
 }
