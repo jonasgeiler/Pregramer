cb3c52fb1385ee246946882d56c61955bee4ac13
Skayo <jonas@skayo.dev>
2021-03-27 15:54:22 +0100

    Improved routing

--- app/config/routes.php
+++ app/config/routes.php
@@ -4,4 +4,4 @@ require __DIR__ . '/error.php';
 
 Flight::route('GET /', 'Home::index');
 Flight::route('GET /privacy', 'Privacy::index');
-Flight::route('GET /@shortcode', 'Post::show');
+Flight::route('GET /@shortcode:[0-9A-Za-z_-]+', 'Post::show');
