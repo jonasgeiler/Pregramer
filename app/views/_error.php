748e818842f7bc1269f692a58793e7c1b101943b
Skayo <jonas@skayo.dev>
2021-03-27 16:23:09 +0100

    Reformat headers and added theme-color meta tag

--- app/views/_error.php
+++ app/views/_error.php
@@ -1,17 +1,20 @@
 <!DOCTYPE html>
 <html>
 	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="theme-color" content="#0096bf" />
 
 		<title><?= $title ?> - Pregramer, by Skayo</title>
 
-		<link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
-		<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
-		<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
-		<link rel="manifest" href="/manifest.json">
+		<link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+		<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
+		<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
+		<link rel="manifest" href="/manifest.json" />
 
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+		<script async defer data-domain="pregramer.link" src="https://analytics.skayo.dev/js/plausible.js"></script>
+
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
 		<style type="text/css">
 			body {
 				height: 100vh;
--- app/views/privacy.php
+++ app/views/privacy.php
@@ -1,15 +1,16 @@
 <!DOCTYPE html>
 <html lang="en">
 	<head>
-		<meta charset="utf-8">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="theme-color" content="#0096bf" />
 
 		<title>Privacy Policy - Pregramer, by Skayo</title>
 
-		<link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png">
-		<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png">
-		<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png">
-		<link rel="manifest" href="/manifest.json">
+		<link rel="apple-touch-icon" sizes="180x180" href="/img/apple-touch-icon.png" />
+		<link rel="icon" type="image/png" sizes="32x32" href="/img/favicon-32x32.png" />
+		<link rel="icon" type="image/png" sizes="16x16" href="/img/favicon-16x16.png" />
+		<link rel="manifest" href="/manifest.json" />
 
 		<meta name="title" content="Privacy Policy - Pregramer, by Skayo" />
 		<meta name="description" content="Privacy Policy for Pregramer" />
@@ -29,7 +30,7 @@
 
 		<script async defer data-domain="pregramer.link" src="https://analytics.skayo.dev/js/plausible.js"></script>
 
-		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css">
+		<link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/water.css@2/out/water.css" />
 	</head>
 	<body>
 		<main>
